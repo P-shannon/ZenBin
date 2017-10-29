1550a6a52ed04e3983accf92b11376458db43d89
p-shannon <a.entyshak@gmail.com>
2017-10-29 15:03:32 -0400

    Adds the zen model to the fray

---
+++ models/zen.js
@@ -0,0 +1,36 @@
+////Grab our database for querying
+const db = require('../db/config');
+
+////Start making the model for zens!11!!11!
+//set the object with a CAPITAL FUCKING LETTER!
+const Zen = {};
+
+//TODO: loop around and give these more complicated methods
+
+//logic for grabbing all zens
+Zen.findAll = function(){
+	return db.query(`
+		SELECT * FROM zens
+		`);
+}
+
+//Logic for grabbing a zen by it's ID
+Zen.findById = function(id){
+	return db.one(`
+		SELECT * FROM zens WHERE
+		id = $1
+		`,[id]);
+}
+
+//Logic for creating a zen
+Zen.create = function(zen){
+	return db.one(`
+		INSERT INTO zens 
+		(content, time_stamp, user_id)
+		VALUES($1, $2, $3)
+		RETURNING *
+		`,[zen.content, zen.timestamp, zen.uid])
+}
+
+//export it
+module.exports = Zen;
